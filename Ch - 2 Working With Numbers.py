3ea4e8fab323221e85594bff5b195dc5ae9a20d6
Vel Prakash <velprakashr08@gmail.com>
2026-04-03 12:11:21 +0530

    feat: add basic numeric print examples in Python

---
+++ Ch - 2 Working With Numbers.py
@@ -0,0 +1,15 @@
+print(2.034)
+print(3 * (4.5 + 3.5))
+print(10 % 3)
+num = 6
+print(str(num) + " is a cool number")
+x = -5
+print(abs(x))
+print(pow(4,2))
+print(max(4,6))
+print(min(3,6))
+print(round(5.5))
+from math import *
+print(floor(3.9))
+print(ceil(5.01))
+print(sqrt(81))
