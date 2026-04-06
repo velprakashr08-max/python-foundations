73cf422372a834c287aa69e09a17ae9e7d992a01
Vel Prakash <velprakashr08@gmail.com>
2026-04-06 14:32:10 +0530

    feat: add user input handling (Chapter 3)

--- Ch - 03 Getting Inputs From Users.py
+++ Ch - 03 Getting Inputs From Users.py
@@ -1,4 +1,4 @@
 name = input("Enter your name: ")
 age = input("Enter your age: ")
 hobby = input("Enter your favourite hobby: ")
-print("Hello " + name + "! You are " + age + " and your favourite hobby is " + hobby) 
+print("Hello " + name + "! You are " + age + " and your favourite hobby is " + hobby)
