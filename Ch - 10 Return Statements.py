cb444229c5c0f75e05bb48106c116424cc4a9e3f
Vel Prakash <velprakashr08@gmail.com>
2026-04-06 14:15:57 +0530

    feat: implement return statements in functions (Chapter 10)

---
+++ Ch - 10 Return Statements.py
@@ -0,0 +1,13 @@
+def cube(num):
+    return num*num*num
+print(cube(3))
+
+def cube(num):
+    return num*num*num
+result = cube(4)
+print(result)
+
+def square(num):
+    return num*num
+solution = square(10)
+print(solution)
