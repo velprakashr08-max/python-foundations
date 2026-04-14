608aa41a6e3037077f93b4a790324ff18c86826c
Vel Prakash <velprakashr08@gmail.com>
2026-04-14 12:32:48 +0530

    feat: implement for loops and iteration (Chapter 18)

---
+++ Ch - 18 For Loops.py
@@ -0,0 +1,21 @@
+for letters in "python":
+    print(letters)
+
+friends = ["Jim", "Jacob", "Jake"]
+for friend in friends:
+    print(friend)
+
+for index in range(10):
+    print(index)
+
+for friend in range(len(friends)):
+    print(friends[friend])
+
+for index in range(3, 10):
+    print(index)
+
+for index in range(5):
+    if index == 0:
+        print("First Iteration")
+    else:
+        print("Not first")
