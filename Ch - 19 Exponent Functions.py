3bba85b4eb07a70ed118695ea756b237f99f1bbf
Vel Prakash <velprakashr08@gmail.com>
2026-04-14 13:14:25 +0530

    feat: implement power function using loop (Chapter 19)

---
+++ Ch - 19 Exponent Functions.py
@@ -0,0 +1,12 @@
+print(2**3)
+
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 3))
+print(raise_to_power(4, 3))
+print(raise_to_power(2, 2))
+print(raise_to_power(3, 1))
