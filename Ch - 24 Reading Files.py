09e0f1cbbf7ec18399fe68da87c083cb5e9ee2df
Vel Prakash <velprakashr08@gmail.com>
2026-04-23 14:15:53 +0530

    feat: add try-except exception handling (chapter 23)

--- Ch - 24 Reading Files.py
+++ Ch - 24 Reading Files.py
@@ -1,28 +1,29 @@
-empolyee_file = open("empolyees.txt", "r")
+empolyee_file = open("Ch - 24 empolyees.txt", "r")
 print(empolyee_file.readable())
 empolyee_file.close()
 
-empolyee_file = open("empolyees.txt", "r")
+empolyee_file = open("Ch - 24 empolyees.txt", "r")
 print(empolyee_file.readline())
 empolyee_file.close()
 
-empolyee_file = open("empolyees.txt", "r")
+empolyee_file = open("Ch - 24 empolyees.txt", "r")
 print(empolyee_file.read())
 empolyee_file.close()
 
-empolyee_file = open("empolyees.txt", "r")
+empolyee_file = open("Ch - 24 empolyees.txt", "r")
 print(empolyee_file.readlines())
 empolyee_file.close()
 
-empolyee_file = open("empolyees.txt", "r")
+empolyee_file = open("Ch - 24 empolyees.txt", "r")
 print(empolyee_file.readline())
 print(empolyee_file.readline())
 empolyee_file.close()
 
-empolyee_file = open("employees.txt", "r")
-print(empolyee_file.readlines()[[2]])
+empolyee_file = open("Ch - 24 empolyees.txt", "r")
+print(empolyee_file.readlines()[2])
 empolyee_file.close()
 
-empolyee_file = open("empolyees.txt", "w")
+empolyee_file = open("Ch - 24 empolyees.txt", "w")
 print(empolyee_file.readable())
 empolyee_file.close()
+# Note: Using 'w' mode will overwrite the file and delete any existing content in "Ch - 24 employees.txt"
