789cbe18f65ae7e4e55e4ed7118bed26c6ac0913
Vel Prakash <velprakashr08@gmail.com>
2026-04-24 13:19:37 +0530

    feat: add file writing operations (Chapter - 25)

---
+++ Ch - 25 Writing to Files.py
@@ -0,0 +1,15 @@
+empolyee_file = open("Ch - 24 empolyees.txt", "a")
+empolyee_file.write("\nHenry = Security")
+empolyee_file.close()
+
+empolyee_file = open("Ch - 24 empolyees.txt", "a")
+empolyee_file.write("\nMichael = Customer Service")
+empolyee_file.close()
+
+empolyee_file = open("Ch - 24 empolyees1.txt", "w")
+empolyee_file.write("Oliver = Customer Service")
+empolyee_file.close()
+
+empolyee_file = open("index.html", "w")
+empolyee_file.write("<p>This is HTML</p>")
+empolyee_file.close()
