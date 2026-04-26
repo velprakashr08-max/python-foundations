4cce59907d11552a3e23bcd6575032495f9941f6
Vel Prakash <velprakashr08@gmail.com>
2026-04-26 14:20:15 +0530

    feat: add Classes and Objects implementation (Chapter - 27)

---
+++ Ch - 27 Classes and Objects.py
@@ -0,0 +1,11 @@
+from Ch_27_Students import Student
+
+student1 = Student("Oliver", "Engineering", "3.7", False)
+print(student1.name)
+print(student1.is_on_probation)
+print(student1.major)
+
+student2 = Student("Pam", "Business", "2.1", "True")
+print(student2.name)
+print(student2.gpa)
+print(student2.is_on_probation)
